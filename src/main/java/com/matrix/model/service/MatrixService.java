a59703dd4c6f888c3d2dcf86248e7e69534281ae
enockpinheiro <enockpinheiro@192.168.1.104>
2019-10-24 20:24:25 -0300

    adjustment

--- src/main/java/com/matrix/model/service/MatrixService.java
+++ src/main/java/com/matrix/model/service/MatrixService.java
@@ -1,12 +1,16 @@
 package com.matrix.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import com.matrix.model.Matrix;
 import com.matrix.repository.MatrixRepository;
 
+@Service
 public class MatrixService extends AbstractService<Matrix>{
 
+	@Autowired
 	private MatrixRepository repository;
 	
 	@Override
