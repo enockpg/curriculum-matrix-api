da2803c3e1e98fc232f203945b0973cb042bdcd8
enockpinheiro <enockpinheiro@192.168.1.104>
2019-10-24 21:40:58 -0300

    include @NoArgsConstructor

--- src/main/java/com/matrix/model/Matrix.java
+++ src/main/java/com/matrix/model/Matrix.java
@@ -12,11 +12,13 @@ import org.hibernate.validator.constraints.Length;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Matrix extends AbstractModel<Long> {
 
